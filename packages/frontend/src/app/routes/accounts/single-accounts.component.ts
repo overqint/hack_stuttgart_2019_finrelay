1ad2e8afe1b82202bb76a5d1782d7f87baebc2f7
Ralph Greschner <ralph.greschner@gmail.com>
2019-10-25 23:22:35 +0200

    feat(frontend): mock account service, single account

--- packages/frontend/src/app/routes/accounts/single-accounts.component.ts
+++ packages/frontend/src/app/routes/accounts/single-accounts.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { ActivatedRoute } from '@angular/router';
+import { AccountsService } from '@shared/accounts.service';
 
 @Component({
   selector: 'app-single-account',
   templateUrl: './single-account.component.html',
 })
 export class SingleAccountComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private accountsService: AccountsService, private route: ActivatedRoute) {}
+
+  accountId: string;
+  transactions: any[];
+  account: any;
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.accountId = params['accountId'];
+    this.route.params.subscribe(async params => {
+      const accountId = params.accountId;
+      this.account = await this.accountsService.findById(accountId);
+      this.transactions = await this.accountsService.getTransactionsByAccountId(this.accountId);
     });
   }
 
   onClickButton() {
     alert('Hello!');
   }
-
-  accountId: string;
 }
