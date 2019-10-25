93cf1003e5872cc91587a984815d685c160e04ca
Ralph Greschner <ralph.greschner@gmail.com>
2019-10-25 23:31:32 +0200

    feat(backend): accounts controller

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { DeutscheBankModule } from './deutsche-bank/deutsche-bank.module';
 import { TransactionProcessorModule } from './transaction-processor/transaction-processor.module';
 import { N8NWrapperModule } from './n8n-wrapper/n8n-wrapper.module';
+import { AccountsController } from './accounts/accounts.controller';
 
 @Module({
   imports: [DeutscheBankModule, N8NWrapperModule],
-  controllers: [AppController],
+  controllers: [AppController, AccountsController],
   providers: [AppService],
 })
 export class AppModule {}
