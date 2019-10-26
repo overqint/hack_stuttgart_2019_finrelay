310440b2f0d0355adf63d38716ad9d4c0320b891
Selim Erunkut <se@novemgold.com>
2019-10-27 01:19:05 +0200

    dashboard configuration
    added different gadgets to our dashboard

--- packages/frontend/src/app/routes/dashboard/dashboard.component.ts
+++ packages/frontend/src/app/routes/dashboard/dashboard.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+  constructor(private http: _HttpClient) {}
 
-  constructor(private http: _HttpClient) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onClickButton() {
     alert('Hello!');
+  }
+  deadline = Date.now() + 200000 * 60 * 60 * 24 * 2 + 1000 * 30;
+
+  listDataMap = {
+    eight: [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' },
+    ],
+    ten: [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' },
+      { type: 'error', content: 'This is error event.' },
+    ],
+    eleven: [
+      { type: 'warning', content: 'This is warning event' },
+      { type: 'success', content: 'This is very long usual event........' },
+      { type: 'error', content: 'This is error event 1.' },
+      { type: 'error', content: 'This is error event 2.' },
+      { type: 'error', content: 'This is error event 3.' },
+      { type: 'error', content: 'This is error event 4.' },
+    ],
+  };
+
+  getMonthData(date: Date): number | null {
+    if (date.getMonth() === 8) {
+      return 1394;
+    }
+    return null;
   }
 }
